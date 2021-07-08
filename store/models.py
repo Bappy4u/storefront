f5309de2bda1a7e03f308d3103e1d011e5b61e88
Najmul H. Bappy <http.bappy@gmail.com>
2021-07-09 03:50:36 +0600

    Removed class meta of customer models in the store app.

--- store/models.py
+++ store/models.py
@@ -44,12 +44,6 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
-    class Meta:
-        db_table = 'store_customers'
-        indexes = [
-            models.Index
-        ]
-
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
