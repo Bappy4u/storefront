00116baa02f5fe0adfc590fa036188a1ec25a501
Najmul H. Bappy <http.bappy@gmail.com>
2021-07-10 05:57:21 +0600

    git

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,13 @@
+from typing_extensions import ParamSpecKwargs
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def hello(request):
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        pass
 
     return render(request, 'hello.html')
