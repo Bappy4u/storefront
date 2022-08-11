d026b96becdadb431c400130f70d4f1ca3cc7892
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-12 02:57:49 +0600

    updated django-rest-framework

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('products', views.products)
+    path('products/', views.product_list),
+    path('products/<int:id>', views.product_view),
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rest_framework import response
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
-def products(request):
-    return HttpResponse('Ok 200');
+
+@api_view()
+def product_list(request):
+    return Response('Ok 200');
+
+
+@api_view()
+def product_view(request, id):
+    return Response(f"product_id={id}")
 
